cc16015a118299e74ab8471300a58d2800963e0c
Wesley Matos <wrickee@gmail.com>
2020-01-29 18:51:29 -0300

    write principal containers of layout

--- src/layouts/Default/index.js
+++ src/layouts/Default/index.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Menu, Close } from '@material-ui/icons';
 
-import { Wrapper } from './styles';
+import {
+  Wrapper, Sidebar, Header, Button, ButtonContainer, PageContainer, Container,
+} from './styles';
 
-const Default = ({ Page, ...props }) => (
-  <Wrapper>
-    <Page {...props} />
-  </Wrapper>
-);
+const Default = ({ Page, ...props }) => {
+  const [showSidebar, setShowSidebar] = useState(window.innerWidth > 768);
+  const validateSidebarWidth = () => {
+    setShowSidebar(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', validateSidebarWidth);
+
+    return () => window.removeEventListener('resize', validateSidebarWidth);
+  }, []);
+
+  return (
+    <Wrapper>
+      <Sidebar show={showSidebar}>
+        <h1> TradersClub </h1>
+      </Sidebar>
+      <Container>
+        <Header>
+          <ButtonContainer>
+            <Button onClick={() => setShowSidebar(!showSidebar)}>
+              {
+                !showSidebar ? <Menu /> : <Close />
+              }
+            </Button>
+          </ButtonContainer>
+        </Header>
+        <PageContainer>
+          <Page {...props} />
+        </PageContainer>
+      </Container>
+    </Wrapper>
+  );
+};
 
 Default.propTypes = {
   Page: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
--- src/layouts/Default/styles.js
+++ src/layouts/Default/styles.js
@@ -1,10 +1,85 @@
 import styled from 'styled-components';
+
 import colors from '../../styles/colors';
+import { minWidth, maxWidth } from '../../styles/devices';
+
+import tcLogo from '../../assets/logo-tc.png';
+import car from '../../assets/car-wireframe.png';
+import sizes from '../../styles/sizes';
+
+const SIDEBAR_WIDTH = '252px';
+const HEADER_HEIGHT = '94px';
 
 export const Wrapper = styled.div`
+  display: flex;
   height: 100%;
   width: 100%;
   background: ${colors.wrapper};
+  position: relative;
+`;
+
+export const Sidebar = styled.aside`
+  height: 100%;
+  background: ${colors.sidebar};
+  min-width: ${SIDEBAR_WIDTH};
+  display: flex;
+  justify-content: center;
+
+  > h1 {
+    margin-top: 37px;
+    display: inline-block;
+    background: url(${tcLogo}) no-repeat;
+    background-size: 70px;
+    width: 70px;
+    height: 70px;
+    color: transparent;
+  }
+
+  @media ${maxWidth.tablet.replace(/\d+/, sizes.tablet - 1)} {
+    ${(props) => `${!props.show ? 'display: none;' : null}`}
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  background: ${colors.header};
+  height: ${HEADER_HEIGHT};
+  top: 0;
+`;
+
+export const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: transparent;
+`;
+
+export const ButtonContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  @media ${minWidth.tablet} {
+    display: none;
+  }
+`;
+
+export const PageContainer = styled.div`
+  width: 100%;
+  flex-grow: 1;
+  background: linear-gradient(${colors.header.concat('E6')}, ${colors.header.concat('E6')}),
+    url(${car}) no-repeat center;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media ${minWidth.tablet.replace(/\d+/, sizes.tablet + 1)} {
+    width: calc(100% - ${SIDEBAR_WIDTH});
+  }
 `;
 
 export const x = {};
