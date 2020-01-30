ac63cfa6a32a7f7536e5d826075b007dbec4a099
Wesley Matos <wrickee@gmail.com>
2020-01-30 18:26:50 -0300

    create header component

---
+++ src/components/layouts/Header/index.js
@@ -0,0 +1,83 @@
+import React, { useState, useCallback } from 'react';
+import { Menu, Close, Search } from '@material-ui/icons';
+import { Formik } from 'formik';
+
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Wrapper,
+  Button,
+  ButtonContainer,
+  Input,
+  InputContainer,
+  ButtonInputContainer,
+} from './styles';
+import colors from '../../../styles/colors';
+import { greaterThan } from '../../../helpers/sizes';
+import { Types } from '../../../store/ducks/sidebar';
+
+const Header = () => {
+  const { laptop: gtLaptop } = greaterThan;
+  const [isGreaterThanLaptop, setIsGreaterThanLaptop] = useState(gtLaptop());
+  const formik = {
+    initialValues: {
+      search: '',
+    },
+    onSubmit: () => null,
+  };
+  const showingSidebar = useSelector((state) => state.sidebar.showing);
+  const dispatcher = useDispatch();
+
+  useCallback(() => {
+    const setShowing = (s) => {
+      dispatcher({ type: Types.SET_SHOWING, showing: s });
+    };
+    const validateSidebarWidth = () => {
+      setIsGreaterThanLaptop(gtLaptop());
+      setShowing(isGreaterThanLaptop);
+    };
+    window.addEventListener('resize', validateSidebarWidth);
+
+    return () => window.removeEventListener('resize', validateSidebarWidth);
+  }, [dispatcher, gtLaptop, isGreaterThanLaptop]);
+
+  return (
+    <Wrapper>
+      <ButtonContainer hideOnLaptop={isGreaterThanLaptop}>
+        <Button onClick={() => dispatcher({ type: Types.SET_SHOWING, showing: !showingSidebar })}>
+          {
+        !showingSidebar ? <Menu /> : <Close />
+      }
+        </Button>
+      </ButtonContainer>
+      <Formik
+        onSubmit={formik.onSubmit}
+        initialValues={formik.initialValues}
+      >
+        {(formProps) => (
+          <InputContainer
+            hideOnMobile={showingSidebar}
+            onSubmit={formProps.handleSubmit}
+          >
+            <Input
+              name="search"
+              type="text"
+              onChange={formProps.handleChange}
+              onBlur={formProps.handleBlur}
+              value={formProps.values.search}
+            />
+            <ButtonInputContainer type="submit">
+              <Search />
+            </ButtonInputContainer>
+          </InputContainer>
+        )}
+      </Formik>
+      <ButtonContainer hideOnMobile={showingSidebar}>
+        <Button bold bgColor="white" width="auto" color={colors.header}>
+      CADASTRAR
+        </Button>
+      </ButtonContainer>
+    </Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/layouts/Header/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+import colors from '../../../styles/colors';
+import { minWidth, maxWidth } from '../../../styles/devices';
+
+export const CONTAINER_HEIGHT = '94px';
+export const DEFAULT_SIDEBAR_ELEMENTS_SIZE = '45px';
+
+
+export const Wrapper = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  background: ${colors.header};
+  height: ${CONTAINER_HEIGHT};
+  top: 0;
+`;
+
+export const Button = styled.button`
+  width: ${(props) => props.width || DEFAULT_SIDEBAR_ELEMENTS_SIZE};
+  height: ${(props) => props.height || DEFAULT_SIDEBAR_ELEMENTS_SIZE};
+  background: ${(props) => props.bgColor || 'transparent'};
+  font-weight: ${(props) => (props.bold ? '700' : '')};
+  color: ${(props) => props.color};
+  padding: 10px;
+  border-radius: 3px;
+`;
+
+export const ButtonContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  @media ${maxWidth.mobileXL} {
+    display: ${(props) => (props.hideOnMobile && 'none')};
+  }
+  @media ${minWidth.laptop} {
+    display: ${(props) => props.hideOnLaptop && 'none'};
+  }
+`;
+
+export const Input = styled.input`
+  height: ${DEFAULT_SIDEBAR_ELEMENTS_SIZE};
+  border-radius: 5px;
+  padding: 0 5px;
+  width: 100%;
+  background-color: transparent;
+  border: white solid 2px;
+`;
+
+export const InputContainer = styled.form`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  width: 50%;
+  position: relative;
+  @media ${maxWidth.mobileXL} {
+    display: ${(props) => (props.hide ? 'none' : '')};
+  }
+  @media ${minWidth.tablet} {
+    width: 70%;
+  }
+`;
+
+export const ButtonInputContainer = styled(Button)`
+  position: absolute;
+  right: 0;
+  background-color: white;
+  color: ${colors.header};
+  > svg {
+    fill: ${colors.header};
+  }
+`;
