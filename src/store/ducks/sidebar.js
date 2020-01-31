da51c65509c587349c2d43be32c4909e4822c333
Wesley Matos <wrickee@gmail.com>
2020-01-31 02:47:28 -0300

    use 'seamless-immutable' in sidebar duck

--- src/store/ducks/sidebar.js
+++ src/store/ducks/sidebar.js
@@ -1,18 +1,16 @@
 import { createActions, createReducer } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
 
 export const { Types, Creators } = createActions({
   setShowing: ['boolean'],
 });
 
 
-const INITIAL_STATE = {
+const INITIAL_STATE = Immutable({
   showing: false,
-};
-
-const setShowing = (state = INITIAL_STATE, { showing }) => ({
-  ...state,
-  showing,
 });
+
+const setShowing = (state = INITIAL_STATE, { showing }) => state.merge({ showing });
 
 export default createReducer(INITIAL_STATE, {
   [Types.SET_SHOWING]: setShowing,
