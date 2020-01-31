502e72d773fecb3e743b8058f58cbd9b9bbde067
Wesley Matos <wrickee@gmail.com>
2020-01-31 02:52:35 -0300

    use 'HANDLERS' obj to create reducer

--- src/store/ducks/sidebar.js
+++ src/store/ducks/sidebar.js
@@ -5,13 +5,14 @@ export const { Types, Creators } = createActions({
   setShowing: ['boolean'],
 });
 
-
 const INITIAL_STATE = Immutable({
   showing: false,
 });
 
 const setShowing = (state = INITIAL_STATE, { showing }) => state.merge({ showing });
 
-export default createReducer(INITIAL_STATE, {
+const HANDLERS = {
   [Types.SET_SHOWING]: setShowing,
-});
+};
+
+export default createReducer(INITIAL_STATE, HANDLERS);
