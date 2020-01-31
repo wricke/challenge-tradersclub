1b4a97db1099a2783fd63ead0ed5278b1ba9786d
Wesley Matos <wrickee@gmail.com>
2020-01-31 01:59:58 -0300

    write mapper file

---
+++ src/helpers/mappers.js
@@ -0,0 +1,39 @@
+export const toOurCar = (car) => {
+  const formatter = (key, value) => {
+    const needFormatation = {
+      km: (v) => String(v).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, '$1.').concat(' KM'),
+    };
+
+    return (needFormatation[key] && needFormatation[key](value)) || value;
+  };
+  const properties = ['model', 'brand', 'km'];
+
+  return Object.entries(car)
+    .reduce(
+      (acc, [key, value]) => {
+        if (properties.includes(key)) {
+          const currentProperties = acc.properties.concat(
+            properties.includes(key) && formatter(key, value),
+          );
+          const current = {
+            properties: currentProperties.filter((x) => x),
+          };
+
+          return {
+            ...acc,
+            ...current,
+          };
+        }
+
+        return {
+          ...acc,
+          [key]: formatter(key, value),
+        };
+      },
+      { properties: [] },
+    );
+};
+
+export const allCarsToOurCars = (cars) => cars.map(toOurCar);
+
+export const fromApi = (data) => data.cars;
