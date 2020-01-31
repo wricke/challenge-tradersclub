48477fc88a458686fc842c12a2d3c1b559a3247d
Wesley Matos <wrickee@gmail.com>
2020-01-30 23:41:28 -0300

    use home component in '/' path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Wrapper from './Wrapper';
+import Home from '../pages/Home';
 
 export default () => (
   <Switch>
-    <Wrapper path="/" component={() => <div />} />
+    <Wrapper path="/" exact component={Home} />
   </Switch>
 );
