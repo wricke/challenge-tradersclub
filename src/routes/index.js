8ba1cf39a68fdea9eddb5d9721a1a4cee0f36601
Wesley Matos <wrickee@gmail.com>
2020-01-31 03:12:09 -0300

    create '/list' path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+
 import Wrapper from './Wrapper';
+
 import Home from '../pages/Home';
+import List from '../pages/List';
 
 export default () => (
   <Switch>
     <Wrapper path="/" exact component={Home} />
+    <Wrapper path="/list" exact component={List} />
   </Switch>
 );
