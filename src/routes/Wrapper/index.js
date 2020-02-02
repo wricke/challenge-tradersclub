71b450dd5d0a0e98180cd028954505cfba3364fa
Wesley Matos <wrickee@gmail.com>
2020-02-02 15:30:41 -0300

    add toast container in wrapper

--- src/routes/Wrapper/index.js
+++ src/routes/Wrapper/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import DefaultLayout from '../../layouts/Default';
 
 const Wrapper = ({ component, ...otherProps }) => (
   <Route
     {...otherProps}
-    render={(props) => <DefaultLayout Page={component} {...props} />}
+    render={(props) => (
+      <Fragment>
+        <ToastContainer />
+        <DefaultLayout Page={component} {...props} />
+      </Fragment>
+    )}
   />
 );
 
