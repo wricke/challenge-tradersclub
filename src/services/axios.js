d6bc331def92f7ea9cac601873182bb27a29991e
Wesley Matos <wrickee@gmail.com>
2020-02-02 15:03:20 -0300

    add function to get one car (with id), to get brands and to update car

--- src/services/axios.js
+++ src/services/axios.js
@@ -6,11 +6,18 @@ export const axios = ax.create({
 
 axios.interceptors.response.use(
   (data) => data.data,
-  (error) => Promise.reject(error.response.data),
+  Promise.reject,
 );
 
-export const getCars = (search) => axios.get('/cars', { params: { search } });
+export const getCars = (search = '') => axios.get('/cars', { params: { search } });
+
+export const getCar = (id) => getCars()
+  .then(({ cars }) => cars.find((car) => car.id === parseInt(id, 0)));
+
+export const getBrands = () => axios.get('/brands');
 
 export const createCar = (data) => axios.post('/cars', data);
 
 export const removeCar = (id) => axios.delete(`/cars/${id}`);
+
+export const updateCar = ({ id, ...data }) => axios.put(`/cars/${id}`, data);
