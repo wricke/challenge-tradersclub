f60d4f65ad12ab3132b8dc8e6f33c5408dd186e1
Wesley Matos <wrickee@gmail.com>
2020-02-02 15:31:02 -0300

    add checkAPI method in axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,4 +1,5 @@
 import ax from 'axios';
+import { toast } from 'react-toastify';
 
 export const axios = ax.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api`,
@@ -9,15 +10,19 @@ axios.interceptors.response.use(
   Promise.reject,
 );
 
-export const getCars = (search = '') => axios.get('/cars', { params: { search } });
+export const checkAPI = () => axios.get().catch(() => {
+  toast.error('Houve um erro ao conectar com a API do TradersClub. Por favor, tente novamente mais tarde.');
+});
+
+export const getCars = (search = '') => axios.get('/cars', { params: { search } }).catch(checkAPI);
 
 export const getCar = (id) => getCars()
-  .then(({ cars }) => cars.find((car) => car.id === parseInt(id, 0)));
+  .then(({ cars }) => (cars || []).find((car) => car.id === parseInt(id, 0)));
 
-export const getBrands = () => axios.get('/brands');
+export const getBrands = () => axios.get('/brands').catch(checkAPI);
 
-export const createCar = (data) => axios.post('/cars', data);
+export const createCar = (data) => axios.post('/cars', data).catch(checkAPI);
 
-export const removeCar = (id) => axios.delete(`/cars/${id}`);
+export const removeCar = (id) => axios.delete(`/cars/${id}`).catch(checkAPI);
 
-export const updateCar = ({ id, ...data }) => axios.put(`/cars/${id}`, data);
+export const updateCar = ({ id, ...data }) => axios.put(`/cars/${id}`, data).catch(checkAPI);
